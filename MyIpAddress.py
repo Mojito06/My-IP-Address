edca4c1e1555c6c55cd8213779fc0336dfcaa9be
Mojito06 <107650636+Mojito06@users.noreply.github.com>
2023-01-18 14:44:39 +0100

    Create MyIpAddress.py

---
+++ MyIpAddress.py
@@ -0,0 +1,9 @@
+def whoTheFuckAmI():
+    ip = "None"
+    try:
+        ip = urlopen(Request("https://ifconfig.me")).read().decode().strip()
+    except:
+        pass
+    return ip
+    
+whoTheFuckAmI()
